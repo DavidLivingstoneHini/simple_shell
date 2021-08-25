07667a1438f8e1b165f0a4d5fdb1376fa1ce2b4e
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-08-25 08:18:53 +0000

    Handles output when shell is called outside of shell

--- exit_s.c
+++ exit_s.c
@@ -1,20 +1,56 @@
 #include "shell.h"
 
 /**
-* exit_s - exit from the shell
-* @as: array of arguments
-* @e: array of environment variables
-* Return: 1
+* c_ignore - custom ignores spaces and newlines
+* (e.g. echo "ls\n ls" | ./a.out)
+* @str: envrionmental variables
+* Return: new string
 */
-
-int exit_s(char **as, char **e)
+char *c_ignore(char *str)
 {
-int status;
-(void)e;
+while (*str == ' ' || *str == '\n')
+str++;
+return (str);
+}
 
-stat = 0;
-if (as[1])
-status = _atoi(as[1]);
-free_a(as);
-exit(stat);
+/**
+* non_interactive - handles when user pipes commands into shell via pipeline
+* (e.g. echo "ls/nls -al/n" | ./a.out)
+* @env: envrionmental variables
+*/
+void non_interactive(list_t *env)
+{
+size_t i = 0, n = 0;
+int command_line_no = 0, exit_stat = 0;
+char *command = NULL, *n_command = NULL, **n_line, **token;
+
+i = get_line(&command);
+if (i == 0)
+{
+free(command);
+exit(0);
+}
+n_command = command;
+command = c_ignore(command);
+n_line = _str_tok(command, "\n"); /* tokenize each command string */
+if (n_command != NULL)
+free(n_command);
+n = 0;
+while (n_line[n] != NULL)
+{
+command_line_no++;
+token = NULL; /* tokenize each command in array of commands */
+token = _str_tok(n_line[n], " ");
+exit_stat = built_in(token, env, command_line_no, n_line);
+if (exit_stat)
+{
+n++;
+continue;
+}
+exit_stat = _execve(token, env, command_line_no);
+n++;
+}
+free_double_ptr(n_line);
+free_linked_list(env);
+exit(exit_stat);
 }
