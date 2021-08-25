c5a7f6e64f40267e0768849e93d9ca11add051d6
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-08-25 14:10:41 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,89 @@
 #include "shell.h"
 
 /**
-* main - creates a simple shell
-* @ac: argument count
-* @av: argument vectors
-* @env: environmental variables
-* Return: 0 on success
-*/
-int main(int ac, char **av, char **env)
+ * execute - executes the command
+ * @cmd: command to run
+ * Return: 0 (success) 
+ *        -1 if cmd is exit 
+ *         1 other error
+ */
+int execute(char **cmd)
 {
-(void)ac;
-(void)av;
 
-prompt(env);
+	pid_t child_pid;
+	int status;
 
-return (0);
+	if (strncmp("exit", cmd[0], 4) == 0)
+		return (-1);
+
+	child_pid = fork();
+
+	if (child_pid == -1)
+	{
+		perror("Error");
+		return (1);
+	}
+	else if (child_pid == 0)
+	{
+		if (execve(cmd[0], cmd, NULL) == -1)
+		{
+			perror("Error");
+			exit(-1);
+		}
+	}
+	else
+		wait(&status);
+
+	return (0);
+}
+
+
+/**
+ * main - shell
+ * @argc: number of arguments
+ * @argv: list of command line arguments
+ * Return: 0 
+ *        -1 (error)
+ */
+
+int main(int argc, char **argv)
+{
+
+	int result;
+	char **tokens;
+	size_t bufsize = BUFSIZ;
+	int isPipe = 0;
+	char *buffer;
+
+	if (argc >= 2)
+	{
+	      if (execve(argv[1], argv, NULL) == -1) //incase of no arguements
+		{
+			perror("Error");
+			exit(-1);
+		}
+		return (0);
+	}
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+
+	do {
+		if (isatty(fileno(stdin)))
+		{
+			isPipe = 1;
+			_puts("cisfun#: ");
+		}
+
+		getline(&buffer, &bufsize, stdin);
+		buffer[_strlen(buffer) - 1] = '\0';
+		tokens = stringToTokens(buffer);
+		result = execute(tokens);
+	} while (isPipe && response != -1);
+
+	return (0);
 }
