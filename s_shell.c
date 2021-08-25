d319010f07dc20e4c15e36d4774320991de1de03
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-08-25 08:22:23 +0000

    Handles outline of shell reprompting and execution

--- s_shell.c
+++ s_shell.c
@@ -1,150 +1,131 @@
 #include "shell.h"
 
 /**
- * shell - simple shell
- * @build: input build
- */
-void shell(config *build)
+* ctrl_c - ignore Ctrl-C input and prints prompt again
+* @n: takes in int from signal
+*/
+void ctrl_c(int n)
 {
-	while (true)
-	{
-		checkAndGetLine(build);
-		if (splitString(build) == false)
-			continue;
-		if (findBuiltIns(build) == true)
-			continue;
-		checkPath(build);
-		forkAndExecute(build);
-	}
+(void)n;
+write(STDOUT_FILENO, "\n$ ", 3);
 }
 
 /**
- * checkAndGetLine - check stdin and retrieves next line; handles
- * prompt display
- * @build: input build
- */
-void checkAndGetLine(config *build)
+* built_in - handles builtins (exit, env, cd)
+* @token: user's typed command
+* @env: environmental variable
+* @num: take in nth user command typed to write error message
+* @command: bring in command to free
+* Return: 1 if acted on builtin, 0 if not
+*/
+int built_in(char **token, list_t *env, int num, char **command)
 {
-	register int len;
-	size_t bufferSize = 0;
-	char *ptr, *ptr2;
+int i = 0;
 
-	build->args = NULL;
-	build->envList = NULL;
-	build->lineCounter++;
-	if (isatty(STDIN_FILENO))
-		displayPrompt();
-	len = getline(&build->buffer, &bufferSize, stdin);
-	if (len == EOF)
-	{
-		freeMembers(build);
-		if (isatty(STDIN_FILENO))
-			displayNewLine();
-		if (build->errorStatus)
-			exit(build->errorStatus);
-		exit(EXIT_SUCCESS);
-
-	}
-	ptr = _strchr(build->buffer, '\n');
-	ptr2 = _strchr(build->buffer, '\t');
-	if (ptr || ptr2)
-		insertNullByte(build->buffer, len - 1);
-	stripComments(build->buffer);
+/* if user types "exit", free cmd tokens, and exit */
+if (_strcmp(token[0], "exit") == 0)
+{
+i = __exit(token, env, num, command);
+}
+/* if user types "env", print, free cmd tokens, and reprompt */
+else if (_strcmp(token[0], "env") == 0)
+{
+_env(token, env);
+i = 1;
+}
+/* if user types "cd" , it will change directory */
+else if (_strcmp(token[0], "cd") == 0)
+{
+i = _cd(token, env, num);
+}
+/* if user types "setenv", create or modify linked list node */
+else if (_strcmp(token[0], "setenv") == 0)
+{
+_setenv(&env, token);
+i = 1;
+}
+/* if user types "setenv", remove linked list node */
+else if (_strcmp(token[0], "unsetenv") == 0)
+{
+_unsetenv(&env, token);
+i = 1;
+}
+return (i);
 }
 
 /**
- * stripComments - remove comments from input string
- * @str: input string
- * Return: length of remaining string
- */
-void stripComments(char *str)
+* ignore_space - return string without spaces in front
+* @str: string
+* Return: new string
+*/
+char *ignore_space(char *str)
 {
-	register int i = 0;
-	_Bool notFirst = false;
-
-	while (str[i])
-	{
-		if (i == 0 && str[i] == '#')
-		{
-			insertNullByte(str, i);
-			return;
-		}
-		if (notFirst)
-		{
-			if (str[i] == '#' && str[i - 1] == ' ')
-			{
-				insertNullByte(str, i);
-				return;
-			}
-		}
-		i++;
-		notFirst = true;
-	}
+while (*str == ' ')
+str++;
+return (str);
 }
 
 /**
- * forkAndExecute - fork current build and execute processes
- * @build: input build
- */
-void forkAndExecute(config *build)
+* ctrl_D - exits program if Ctrl-D was pressed
+* @i: characters read via get_line
+* @command: user's typed in command
+* @env: environmental variable linked list
+*/
+void ctrl_D(int i, char *command, list_t *env)
 {
-	int status;
-	pid_t f1 = fork();
-
-	convertLLtoArr(build);
-	if (f1 == -1)
-	{
-		perror("error\n");
-		freeMembers(build);
-		freeArgs(build->envList);
-		exit(1);
-	}
-	if (f1 == 0)
-	{
-		if (execve(build->fullPath, build->args, build->envList) == -1)
-		{
-			errorHandler(build);
-			freeMembers(build);
-			freeArgs(build->envList);
-			if (errno == ENOENT)
-				exit(127);
-			if (errno == EACCES)
-				exit(126);
-		}
-	} else
-	{
-		wait(&status);
-		if (WIFEXITED(status))
-			build->errorStatus = WEXITSTATUS(status);
-		freeArgsAndBuffer(build);
-		freeArgs(build->envList);
-	}
+if (i == 0) /* handles Ctrl+D */
+{
+free(command); /* exit with newline if in shell */
+free_linked_list(env);
+if (isatty(STDIN_FILENO))/* ctrl+d prints newline */
+	write(STDOUT_FILENO, "\n", 1);
+exit(0);
+}
 }
 
 /**
- * convertLLtoArr - convert linked list to array
- * @build: input build
- */
-void convertLLtoArr(config *build)
+* prompt - repeatedly prompts user and executes user's cmds if applicable
+* @en: envrionmental variables
+* Return: 0 on success
+*/
+int prompt(char **en)
 {
-	register int i = 0;
-	size_t count = 0;
-	char **envList = NULL;
-	linked_l *tmp = build->env;
+list_t *env;
+size_t i = 0, n = 0;
+int command_line_no = 0, exit_stat = 0;
+char *command, *n_command, **token;
 
-	count = list_len(build->env);
-	envList = malloc(sizeof(char *) * (count + 1));
-	if (!envList)
-	{
-		perror("Malloc failed\n");
-		exit(1);
-	}
-	while (tmp)
-	{
-		envList[i] = _strdup(tmp->string);
-		tmp = tmp->next;
-		i++;
-	}
-	envList[i] = NULL;
-	build->envList = envList;
+env = env_linked_list(en);
+do {
+command_line_no++;
+if (isatty(STDIN_FILENO)) /* reprompt if in interactive shell */
+write(STDOUT_FILENO, "$ ", 2);
+else
+non_interactive(env);
+signal(SIGINT, ctrl_c); /* makes ctrl+c not work */
+command = NULL;
+i = 0; /* reset vars each time loop runs */
+i = get_line(&command); /* read user's cmd in stdin */
+ctrl_D(i, command, env); /* exits shell if ctrl-D */
+n_command = command;
+command = ignore_space(command);
+n = 0;
+while (command[n] != '\n') /* replace get_line's \n with \0 */
+n++;
+command[n] = '\0';
+if (command[0] == '\0') /* reprompt if user hits enter only */
+{
+	free(n_command);
+	continue;
+}
+token = NULL;
+token = _str_tok(command, " "); /*token user cmd*/
+if (n_command != NULL)
+free(n_command);
+exit_stat = built_in(token, env, command_line_no, NULL);
+if (exit_stat)
+continue;
+exit_stat = _execve(token, env, command_line_no);
+} while (1); /* keep on repeating till user exits shell */
+return (exit_stat);
 }
